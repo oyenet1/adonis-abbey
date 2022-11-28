59fa6bb2da539608785d2fffbfac96cf2864584a
oyenet1 <bowofadeoyerinde@gmail.com>
2022-11-28 22:17:17 +0100

    first commit

---
+++ database/migrations/2022_10_24_155805_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('authors');
+            $table->string('cover_image');
+            $table->string('isbn');
+            $table->integer('revision_number');
+            $table->string('publisher');
+            $table->string('genre')->nullable();
+            $table->dateTime('published_at');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
