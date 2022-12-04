7d2f8813dfde08cf9bb57426a393e4ae53b9052e
oyenet1 <bowofadeoyerinde@gmail.com>
2022-12-04 16:53:00 +0100

    done

--- database/migrations/2022_10_22_155809_create_users_table.php
+++ database/migrations/2022_10_22_155809_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->bigInteger('phone')->nullable()->unique();
+            $table->string('phone')->nullable()->unique();
             $table->string('username')->unique();
             $table->string('role')->nullable();
             $table->double('balance', 12, 2)->nullable();
